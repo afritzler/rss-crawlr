5d0687eb28744722dac0fb1fb284ebc893e164e6
Andreas Fritzler <andreas.fritzler@gmail.com>
2017-02-16 16:10:28 +0100

    Added initial python skeleton

---
+++ rss-crawlr/crawlr/crawlr.py
@@ -0,0 +1,16 @@
+class Crawlr:
+
+    refresh_rate = 1000
+    rss_feed_file = "./test_config"
+
+    def __init__(self, rss_feed_file, refresh_rate):
+        self.rss_feed_file = rss_feed_file
+        self.refresh_rate = refresh_rate
+
+    def read_config(self):
+        retur True
+
+    def start(self):
+        read_config()
+
+        return "Crawling ..."
---
+++ rss-crawlr/server.py
@@ -0,0 +1,4 @@
+from crawlr import Crawlr
+
+crawlr = Crawlr("./sample_config",1000)
+crawlr.start()
