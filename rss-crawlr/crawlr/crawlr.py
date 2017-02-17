872ab684c74a065f4c4f9bfc4c062baee9dfaf32
Andreas Fritzler <andreas.fritzler@gmail.com>
2017-02-17 17:50:39 +0100

    Changed configuration file structure

--- rss-crawlr/crawlr/crawlr.py
+++ rss-crawlr/crawlr/crawlr.py
@@ -1,14 +1,20 @@
+import yaml
+
 class Crawlr:
 
-    refresh_rate = 1000
-    rss_feed_file = "./test_config"
+    config_file = "./test_config"
 
-    def __init__(self, rss_feed_file, refresh_rate):
-        self.rss_feed_file = rss_feed_file
-        self.refresh_rate = refresh_rate
+    def __init__(self, rss_feed_file):
+        self.config_file = rss_feed_file
 
     def read_config(self):
-        retur True
+        with open(self.config_file, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+
+        for section in cfg:
+            print(section)
+
+        return True
 
     def start(self):
         read_config()
