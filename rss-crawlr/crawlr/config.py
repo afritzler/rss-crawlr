f8cec9fdfc77ae22028be9de96061e6d2d6adc28
Andreas Fritzler <andreas.fritzler@gmail.com>
2017-02-18 11:00:52 +0100

    Restructure config file handling

---
+++ rss-crawlr/crawlr/config.py
@@ -0,0 +1,15 @@
+class Config:
+
+    config_file = "./test_config"
+
+    def __init__(self, rss_feed_file):
+        self.config_file = rss_feed_file
+
+    def read_config(self):
+        with open(self.config_file, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+
+        for section in cfg:
+            print(section)
+
+        return True
--- rss-crawlr/crawlr/crawlr.py
+++ rss-crawlr/crawlr/crawlr.py
@@ -2,21 +2,7 @@ import yaml
 
 class Crawlr:
 
-    config_file = "./test_config"
-
-    def __init__(self, rss_feed_file):
-        self.config_file = rss_feed_file
-
-    def read_config(self):
-        with open(self.config_file, 'r') as ymlfile:
-            cfg = yaml.load(ymlfile)
-
-        for section in cfg:
-            print(section)
-
-        return True
-
     def start(self):
-        read_config()
+        config = Config("./test-config")
 
         return "Crawling ..."
