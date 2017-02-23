dca4d369368c99671b600ea0a74e5bbca0274eb5
Andreas Fritzler <andreas.fritzler@gmail.com>
2017-02-23 19:11:39 +0100

    Added cassandra settings

--- rss-crawlr/crawlr/crawlr.py
+++ rss-crawlr/crawlr/crawlr.py
@@ -1,4 +1,5 @@
 from config import Config
+from cassandra.cluster import Cluster
 import feedparser
 import redis
 import os
@@ -8,9 +9,12 @@ class Crawlr:
 
     def __init__(self, rss_feed_file):
         self.config_file = rss_feed_file
+        print "Creating redis cache connection ..."
         self.cache = redis.StrictRedis(host=os.getenv('REDIS_HOST', 'localhost'),
                                        port=os.getenv('REDIS_POST', 6379),
                                        db=0)
+        print "Creating cassandra cluster connection ..."
+        self.cluster = Cluster([os.getenv('CASSANDRA _HOST', 'localhost')])
 
     def start(self):
         print "Starting crawlr ..."
---
+++ rss-crawlr/run_server.sh
@@ -0,0 +1,6 @@
+# TODO check for virtualenv setup
+
+source settings
+
+python server.py config/crawlr.conf
+
