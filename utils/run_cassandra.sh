13e1958db6c2fb2fca7c939f4c75295438c5a4e2
Andreas Fritzler <andreas.fritzler@gmail.com>
2017-05-03 13:02:54 +0200

    Use host network binding for docker services

--- utils/run_cassandra.sh
+++ utils/run_cassandra.sh
@@ -4,4 +4,4 @@ app="cassandra"
 if docker ps | awk -v app="app" 'NR>1{  ($(NF) == app )  }'; then
   docker stop "$app" && docker rm -f "$app"
 fi
-docker run --name cassandra -d cassandra:3
+docker run --net=host --name cassandra -d cassandra:3
--- utils/run_redis.sh
+++ utils/run_redis.sh
@@ -5,4 +5,4 @@ if docker ps | awk -v app="app" 'NR>1{  ($(NF) == app )  }'; then
   docker stop "$app" && docker rm -f "$app"
 fi
 
-docker run --name redis -d redis:alpine
+docker run --net=host --name redis -d redis:alpine
